7f0c842eaadcdc5cfa8ada8a55cee077890dec86
Mohammed Imran Arif k <127063631+Mohammed-imran-arif@users.noreply.github.com>
2024-06-04 18:23:05 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,23 +1,63 @@
 /* AIzaSyDTiMhdanoRzUHmHVanuK_a61Nup8I7cU0 */
 
-import { GoogleGenerativeAI } from "@google/generative-ai";
+/* AIzaSyDTiMhdanoRzUHmHVanuK_a61Nup8I7cU0 */
 
-// Access your API key (see "Set up your API key" above)
-const API_KEY = "AIzaSyDTiMhdanoRzUHmHVanuK_a61Nup8I7cU0"
-const genAI = new GoogleGenerativeAI(API_KEY);
 
-async function run(prompt) {
-  
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
-  
 
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-  return text
+// node --version # Should be >= 18
+// npm install @google/generative-ai
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const MODEL_NAME = "gemini-1.0-pro";
+const API_KEY = "AIzaSyDTiMhdanoRzUHmHVanuK_a61Nup8I7cU0";
+
+async function runChat(prompt) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2048,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+    ],
+  });
+
+  const result = await chat.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+  return response.text();
 }
 
-export default run;
-
+ export default runChat;
